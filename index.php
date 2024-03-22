c1bb007443a2cebc074d98ca8f20ea2f3b9d5e40
nathan-desilva <159185114+nathan-desilva@users.noreply.github.com>
2024-03-22 18:49:57 +0000

    formatted

--- index.php
+++ index.php
@@ -16,25 +16,25 @@
     <title>The Area & Perimeter program,in PHP</title>
   </head>
   <body>
-  <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
-  <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
-    <header class="mdl-layout__header">
-      <div class="mdl-layout__header-row">
-        <span class="mdl-layout-title">The math program, in PHP</span>
-      </div>
-    </header>
-    <main class="mdl-layout__content">
-      <div class="right-image">
-        <img src="./images/area_and_perimeter.png" alt="street image" />
-      </div>
-      <!-- basic text -->
-      <p style="font-size: x-large">The dimensions of a rectangle are: 5 cm and 3 cm.</p>
-      <div class="page-content">
-        <?php echo '<p>The Area is: = ' . 5 * 3 . ' cm². </p>'; ?>
-        <?php echo '<p>The Perimeter is: ' . 2 * (5 + 3) . ' cm. </p>'; ?>
-      </div>
-      <br />
-    </main>
-  </div>
-</body>
+    <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+    <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+      <header class="mdl-layout__header">
+        <div class="mdl-layout__header-row">
+          <span class="mdl-layout-title">The math program, in PHP</span>
+        </div>
+      </header>
+      <main class="mdl-layout__content">
+        <div class="right-image">
+          <img src="./images/area_and_perimeter.png" alt="street image" />
+        </div>
+        <!-- basic text -->
+        <p style="font-size: x-large">The dimensions of a rectangle are: 5 cm and 3 cm.</p>
+        <div class="page-content">
+          <?php echo '<p>The Area is: = ' . 5 * 3 . ' cm². </p>'; ?>
+          <?php echo '<p>The Perimeter is: ' . 2 * (5 + 3) . ' cm. </p>'; ?>
+        </div>
+        <br />
+      </main>
+    </div>
+  </body>
 </html>
