a913d8073164b62f7700ca9641f41f651030e6cf
nathan-desilva <159185114+nathan-desilva@users.noreply.github.com>
2024-03-22 18:49:21 +0000

    changed photo

--- index.php
+++ index.php
@@ -1,21 +1,21 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <meta charset="utf-8" />
-  <meta name="description" content="The Area & Perimeter program, in PHP" />
-  <meta name="keywords" content="mths, ics2o" />
-  <meta name="author" content="Nathan De Silva" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <link rel="stylesheet" href="./css/style.css" />
-  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
-  <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.pink-purple.min.css" />
-  <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png" />
-  <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png" />
-  <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png" />
-  <link rel="manifest" href="site.webmanifest" />
-  <title>The Area & Perimeter program,in PHP</title>
-</head>
-<body>
+    <meta charset="utf-8" />
+    <meta name="description" content="The Area & Perimeter program, in PHP" />
+    <meta name="keywords" content="mths, ics2o" />
+    <meta name="author" content="Nathan De Silva" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="./css/style.css" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+    <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.pink-purple.min.css" />
+    <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png" />
+    <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png" />
+    <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png" />
+    <link rel="manifest" href="site.webmanifest" />
+    <title>The Area & Perimeter program,in PHP</title>
+  </head>
+  <body>
   <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
   <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
     <header class="mdl-layout__header">
@@ -25,7 +25,7 @@
     </header>
     <main class="mdl-layout__content">
       <div class="right-image">
-        <img src="./images/street.jpg" alt="street image" />
+        <img src="./images/area_and_perimeter.png" alt="street image" />
       </div>
       <!-- basic text -->
       <p style="font-size: x-large">The dimensions of a rectangle are: 5 cm and 3 cm.</p>
